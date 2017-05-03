5b51067fd0b515ea60dffc4c22e6c67073113ed3
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 16:08:49 -0300

    commentaries and more verbose

--- run-docker-archiver-db.sh
+++ run-docker-archiver-db.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+#
+# An auxiliary script to start a new Docker container. It should be used to test the 
+# respective image. To deploy, use the composed repository, available in
+# https://github.com/lnls-sirius/docker-alarm-composed .  
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+
 . ./env-vars.sh
 
+# Verifies if a container with the same name is already running.
 CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
 if [ ! -z "$CONTAINERS" ]; then
-    docker stop ${DOCKER_RUN_NAME}
-    docker rm ${DOCKER_RUN_NAME}
+
+    echo "A container with the name ${DOCKER_RUN_NAME} is already running..."
+
+    # Stops running container and deletes it.
+    echo "Executing 'docker stop ${DOCKER_RUN_NAME}' ..."
+    docker stop ${DOCKER_RUN_NAME} &> /dev/null
+
+    echo "Executing 'docker rm ${DOCKER_RUN_NAME}' ..."
+    docker rm ${DOCKER_RUN_NAME} &> /dev/null
 fi
 
 docker run -d --name=${DOCKER_RUN_NAME} -e MYSQL_USER=${MYSQL_USER} -d \
     -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -e MYSQL_DATABASE=${MYSQL_DATABASE} \
-    -e MYSQL_ROOT_PASSWORD,="controle" -p ${MYSQL_PORT}:${MYSQL_PORT} --network=${NETWORK_ID} \
+    -e MYSQL_ROOT_PASSWORD="controle" -p ${MYSQL_PORT}:${MYSQL_PORT} --network=${NETWORK_ID} \
     --volumes-from=${DOCKER_VOLUME_NAME} ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME}
 
 
