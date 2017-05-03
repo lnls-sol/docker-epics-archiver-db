5895646551b3df98e484e5e416da71e95d4b949e
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 16:04:36 -0300

    building network and volume in the same script

--- build-docker-archiver-db.sh
+++ build-docker-archiver-db.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
+#
+# A simple script to build a container for the archiver's database.
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+
 . ./env-vars.sh
 
-./build-volume.sh
+### Creates a new storage volume for the container if it doesn't exist ###
+# Checks if the volume already exists
+DOCKER_DATA_CONTAINERS=$(docker ps -a | grep ${DOCKER_VOLUME_NAME})
+# If not, it is created
+if [ -z "${DOCKER_DATA_CONTAINERS}" ]; then
+    echo 'Storage volume has not been found. Creating a new one ...'
+    docker create -v ${PGDATA} --name ${DOCKER_VOLUME_NAME} postgres &> /dev/null
+fi
 
-./build-network.sh
+### Creates a new internal network if it doesn't exist ###
+DOCKER_NETWORKS=$(docker network ls | grep ${NETWORK_ID})
+if [ -z "${DOCKER_NETWORKS}" ]; then
+    echo 'Creating a new network...'
+    docker network create ${NETWORK_ID} &> /dev/null
+fi
 
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME} .
